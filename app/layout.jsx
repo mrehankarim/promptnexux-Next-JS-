f8c5c94305b20973ec9a67344db884c84a9d11b0
Muhammad Rehan <162461517+mrehankarim@users.noreply.github.com>
2025-02-14 17:35:10 +0500

    setup project and basic frontend

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@/styles/global.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+export const metadata={
+    title:'Promptnexnus',
+    description:'Discover and share AI prompts'
+}
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            <main className='app'>
+                <Nav></Nav>
+                {children}
+            </main>
+        </body>
+      
+    </html>
+  )
+}
+
+export default RootLayout
